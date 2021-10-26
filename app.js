c5e9f64a58e2881ef0e5509ad91677a5a9c97694
AhmedAQ11 <m.ahmedAQ.786@gmail.com>
2021-10-27 01:58:54 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,240 @@
+// var userName = "Ahmed";
+
+// var age = 28;
+
+// alert(userName);
+// alert(age);
+
+////add//
+
+// var num1 = 150;
+// var num2 = 50;
+
+// var add = num1 + num2;
+
+// alert(add);
+
+
+
+///sub///
+
+
+// var num1 = 100;
+// var num2 = 50;
+
+// var sub = num1 - num2;
+
+// alert(sub);
+
+
+///mul///
+
+// var num1 = 11;
+// var num2 = 2;
+
+// var mul = num1 * num2;
+
+// alert(mul);
+
+
+
+
+///div///
+
+
+
+// var num1 = 10;
+// var num2 = 2;
+
+// var div = num1 / num2;
+
+// alert(div);
+
+
+
+///modulus////
+
+
+// var num1 = 11;
+// var num2 = 2;
+
+// var mod = num1 % num2;
+
+// alert(mod);
+
+
+
+// var a = 10 + 20 - 30 *  (10 + 20);
+
+// alert(a);
+
+
+
+
+
+
+
+
+// var a = (10 * 10) + 20 - 30 + (10 - 10) * 2;
+
+// alert(a);
+
+
+
+
+
+// var a = (50 + 10) + 20 + 40 - 20 * 2 + 10 * 2;
+
+// alert(a);
+
+
+
+
+//// POST / PRE increment && POST / PRE decrement////
+
+/// POST increment a++
+/// PRE increment  ++a
+
+
+// var a = 10;
+
+// var b = a++;
+
+// alert(b);
+
+
+// var a = 11;
+
+// var b = a++;
+
+// var c = a;
+
+
+// alert(b);
+// alert(c);
+
+
+
+
+// var a = 11;
+
+// var b = ++a;
+
+// var c = a;
+
+// alert(b);
+// alert(c);
+
+
+// var a = 10;
+
+// var result = a++ + a++ + ++a;
+
+// alert(result); (34)
+
+
+// var a = 15;
+
+
+// var result = a++ + ++a + a + a++;
+
+
+// alert(result); (66)
+
+
+
+// var a = 20;
+
+
+// var result = a++ + a + a + ++a + ++a + a++;
+
+
+// alert(result); (130)
+
+
+
+
+// a--
+// --a
+
+
+
+// var a = 15;
+
+// var result = a++ + ++a - a + a++ + ++a - a - --a;
+
+// alert(result); (14)
+
+
+
+
+// var name = prompt("Name" , "Ahmed");
+
+// document.write(name);
+
+
+// var num1 = prompt ("number 1");
+// var num2 = prompt ("number 2");
+
+// document.write(num1 + num2);
+
+
+
+// var num1 = +prompt ("number 1" , "20");
+// var num2 = +prompt ("number 2" , "30" );
+
+// document.write("ADD <br>" , num1 + num2);
+
+
+
+
+// / ASSIGNMENT JAVA SCRIPT///
+
+// var firstName = prompt("firstName " , "Muhammed <br>");
+
+// var lastName = prompt("lastName" , "Ahmed <br> ") 
+
+// var rollName = prompt("rollName"  ,  "786 <br>" );
+
+// var schoolName = prompt("schoolName" , "JawanPK");
+
+
+// document.write(firstName + lastName + rollName + schoolName );
+
+// var subject1 = +prompt ("Islamiat", "80");
+
+// var subject2 = +prompt ("English" , "60");
+
+// var subject3 = +prompt ("html" , "65");
+
+// var subject4 = +prompt ("Javascript" , "70");
+
+// var subject5 = +prompt ("css" , "75");
+
+
+// document.write("Obt mark <br> " , subject1  + subject2 + subject3 + subject4 + subject5);
+
+// subject2 = +prompt ("Islamiat", "100");
+
+// var subject2 = +prompt ("English" , "100");
+
+// var subject3 = +prompt ("html" , "100");
+
+// var subject4 = +prompt ("Javascript" , "100");
+
+// var subject5 = +prompt ("css" , "100");
+
+
+// document.write ("total <br>" , subject1+ subject2 + subject3 + subject4 + subject5);
+
+// var obt = 350;
+
+// var total = 500;
+
+// var percentage = obt / total * 100;
+
+// alert(percentage);
+
+
+
+
